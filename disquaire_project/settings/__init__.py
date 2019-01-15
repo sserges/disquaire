b4d00275bf03975f539d08d94e3d159c17067a2b
sserges <agounkeserge@gmail.com>
2019-01-15 18:20:46 +0000

    new files to better organize settings

--- disquaire_project/settings/__init__.py
+++ disquaire_project/settings/__init__.py
@@ -21,15 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '4i&u(!%shd*0-3$ls)fohsjsd48t(gu%1-ch_wyzk7@#n3bd8e')
+SECRET_KEY = '4i&u(!%shd*0-3$ls)fohsjsd48t(gu%1-ch_wyzk7@#n3bd8e'
 # '-~aO;| F;rE[??/w^zcumh(9'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['disquaire.herokuapp.com']
@@ -89,9 +86,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
         'NAME': 'disquaire', # le nom de notre base de données créée précédemment
-        'USER': 'celinems', # attention : remplacez par votre nom d'utilisateur !!
-        'PASSWORD': '',
-        'HOST': '',
+        'USER': 'postgres', # attention : remplacez par votre nom d'utilisateur !!
+        'PASSWORD': 'logos01Django',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
@@ -138,12 +135,3 @@ STATIC_URL = '/static/'
 
 # Django debug toolbar
 INTERNAL_IPS = ['127.0.0.1']
-
-
-if os.environ.get('ENV') == 'PRODUCTION':
-
-    STATIC_ROOT = os.path.join(BASE_ROOT, 'staticfiles')
-
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
